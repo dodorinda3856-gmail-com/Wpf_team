c0a558cbf49530894cec4e7f865c39dc18064b7e
devlee77 <tjdcks0928@naver.com>
2021-12-16 15:26:18 +0900

    데이터베이스에서 환자정보 데이터 바인딩 완료

--- AdminProgram/PatientPage.xaml.cs
+++ AdminProgram/PatientPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AdminProgram.ViewModels;
+using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,16 +23,11 @@ namespace AdminProgram
     /// </summary>
     public partial class PatientPage : Page
     {
-        
+        OracleConnection connn;
+
         public PatientPage()
         {
             InitializeComponent();
-
-            List<User> users = new List<User>();
-            users.Add(new User() {Chartnum = 00001, patientnum = 1111, Name = "이성찬", Sex = true, Age = 27, SecurityNum = "9509281000000", Birthday = 950928, RecentVisit = "21/12/15", Address = "서울시 양천구 목동"});
-            users.Add(new User() {Chartnum = 00001, patientnum = 1111, Name = "이성찬", Sex = true, Age = 27, SecurityNum = "9509281000000", Birthday = 950928, RecentVisit = "21/12/15", Address = "서울시 양천구 목동"});
-            users.Add(new User() {Chartnum = 00001, patientnum = 1111, Name = "이성찬", Sex = true, Age = 27, SecurityNum = "9509281000000", Birthday = 950928, RecentVisit = "21/12/15", Address = "서울시 양천구 목동"});
-            dataGrid.ItemsSource = users;
         }
 
         private void selectedDate(object sender, SelectionChangedEventArgs e)
@@ -53,19 +51,78 @@ namespace AdminProgram
             AddPatient addPatient = new();
             addPatient.ShowDialog();
         }
+
+
+        //나이 계산하는 함수
+        private int calculate_age(DateTime date)
+        {
+            int now = 2021;
+            string str_tmp = date.ToString("yyyy");
+            int age = Convert.ToInt32(str_tmp);
+            age = now - age + 1;
+
+            return age;
+        }
+
+        private void Search_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string strCon = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=loonshot.cgxkzseoyswk.us-east-2.rds.amazonaws.com)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ORCL)));User Id=loonshot;Password=loonshot123;";
+                connn = new OracleConnection(strCon);
+                connn.Open();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+
+            string sql = "select PATIENT_ID, RESIDENT_REGIST_NUM, ADDRESS, PATIENT_NAME, PHONE_NUM, REGIST_DATE, GENDER, DOB from PATIENT";
+
+            /* Connection, Command, DataReader를 통한 데이터 추출 */
+            //OracleCommand : SQL 서버에 어떤 명령을 내리기 위해 사용하는 클래스 ===== 명령문 실행 용도
+            //데이타를 가져오거나(SELECT), 테이블 내용을 삽입(INSERT), 갱신(UPDATE), 삭제(DELETE) 하기 위해
+            //이 클래스를 사용할 수 있으며, 저장 프로시져(Stored Procedure)를 사용할 때도 사용
+            OracleCommand comm = new OracleCommand();
+            /*if (conn == null)
+                DBConnection(this, null);*/
+            comm.Connection = connn;
+            comm.CommandText = sql;
+
+            //ExecuteReader : Sends the CommandText to the Connection and builds a SqlDataReader.
+            //SQL Server와 연결을 유지한 상태에서 한번에 한 레코드(One Row)씩 데이타를 가져오는데 사용
+            //DataReader는 하나의 Connection에 하나만 Open되어 있어야 하며, 사용이 끝나면 Close() 메서드를 호출하여 닫아 준다.
+            OracleDataReader reader = comm.ExecuteReader(CommandBehavior.CloseConnection);
+            List<PMModel> datas = new List<PMModel>(); //listView에 데이터 뿌리기 위한 틀
+
+            int a = 0;
+
+            while (reader.Read())// 다음 레코드 계속 가져와서 루핑 - Advances the SqlDataReader to the next record. true if there are more rows; otherwise false.
+            {
+                datas.Add(new PMModel()
+                {
+                    //GetString : Gets the column ordinal, given the name of the column.
+                    //가져올 데이터의 컬럼명을 인수로 넣어줌
+                    Patient_ID = reader.GetInt32(reader.GetOrdinal("PATIENT_ID")),
+                    Resident_Regist_Num = reader.GetString(reader.GetOrdinal("Resident_Regist_Num")), //listView의 textblock id(?)
+                    Address = reader.GetString(reader.GetOrdinal("Address")),
+                    Patient_Name = reader.GetString(reader.GetOrdinal("Patient_Name")),
+                    Phone_Num = reader.GetString(reader.GetOrdinal("Phone_Num")),
+                    Regist_Date = reader.GetDateTime(reader.GetOrdinal("Regist_Date")),
+                    Gender = reader.GetString(reader.GetOrdinal("Gender")),
+                    Dob = reader.GetDateTime(reader.GetOrdinal("Dob")),
+                    Age = calculate_age(reader.GetDateTime(reader.GetOrdinal("Dob")))
+                }) ;
+            }
+            dataGrid.ItemsSource = datas; //listview의 데이터 바인딩 진행
+
+            reader.Close();
+        }
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 
-    internal class User
-    {
-        public int Chartnum { get; set; }
-        public int patientnum { get; set; }
-        public string? Name { get; set; }
-        public bool Sex { get; set; }
-        public int Age { get; set; }
-        public string? SecurityNum { get; set; }
-        public int Birthday { get; set; }
-        public string? RecentVisit { get; set; }
-        public string? Address { get; set; }
-
-    }
 }
