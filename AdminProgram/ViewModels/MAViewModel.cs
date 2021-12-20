a7d4cec0c3c16d78af137e763895570510eca8d6
NaHyeonJeong <pigismile@gmail.com>
2021-12-20 17:50:04 +0900

    hj-viewmodel 추가 시작ㅠㅠ

---
+++ AdminProgram/ViewModels/MAViewModel.cs
@@ -0,0 +1,44 @@
+﻿using AdminProgram.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace AdminProgram.ViewModels
+{
+    public class MAViewModel : ObservableRecipient
+    {
+        // viewmodel에서는 model의 값을 가져와서 view에 뿌리기 전에 전처리 하는 곳
+        // messenger의 send와 receive도 하는 곳
+        // viewmodel은 messenger를 사용하는 통신에 직접적인 영향을 미침
+
+        private readonly ILogger _logger;
+
+        // MAModel을 observable collection 형식(사실상 list)으로 받아옴
+        private ObservableCollection<MAModel> maModel;
+        public ObservableCollection<MAModel> MAModel
+        {
+            get { return maModel; }
+            set { SetProperty(ref maModel, value); }
+        }
+
+        public MAViewModel(ILogger<MAViewModel> logger)
+        {
+            _logger = logger;
+            _logger.LogInformation("{@ILogger}", logger);
+        }
+
+        private void ContentCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ProductOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+
+        }
+
+
+    }
+}
--- AdminProgram/Views/MediAppointment.xaml.cs
+++ AdminProgram/Views/MediAppointment.xaml.cs
@@ -14,30 +14,12 @@ namespace AdminProgram
 
     public partial class MediAppointmentPage : Page
     {
-        //OracleConnection conn;
         string strCon = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=loonshot.cgxkzseoyswk.us-east-2.rds.amazonaws.com)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ORCL)));User Id=loonshot;Password=loonshot123;";
 
         public MediAppointmentPage()
         {
             InitializeComponent();
-            //DBConn();
         }
-
-        /*public void DBConn()
-        {
-            try
-            {
-                string strCon = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=loonshot.cgxkzseoyswk.us-east-2.rds.amazonaws.com)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ORCL)));User Id=loonshot;Password=loonshot123;";
-                conn = new OracleConnection(strCon);
-                conn.Open();
-
-                MessageBox.Show("DB Connection OK...");
-            }
-            catch (Exception err)
-            {
-                MessageBox.Show(err.ToString());
-            }
-        }*/
 
         //혹시나 날짜 값이 필요할 경우 이거를 활용해서 할 것
         private void selectedDate(object sender, SelectionChangedEventArgs e)
@@ -58,30 +40,6 @@ namespace AdminProgram
         //==예약 환자 리스트 업데이트(임시)==//
         private void ReservationUpdateBtn(object sender, RoutedEventArgs e)
         {
-            /*if (conn == null)
-            {
-                DBConn();
-            }
-
-            string sql = "SELECT ms.STAFF_NAME FROM RESERVATION r, MEDI_STAFF ms WHERE r.MEDICAL_STAFF_ID = ms.STAFF_ID ;";
-
-            OracleCommand comm = new OracleCommand();
-            comm.Connection = conn;
-            comm.CommandText = sql;
-
-            OracleDataReader reader = comm.ExecuteReader();
-            List<MAModel> datas = new List<MAModel>();
-
-            while (reader.Read())
-            {
-                datas.Add(new MAModel()
-                {
-                    StaffName = reader.GetString(reader.GetOrdinal("STAFF_NAME"))
-                });
-            }
-            reservationGrid.ItemsSource = datas;
-            reader.Close();*/
-            
             string sql = "SELECT ms.STAFF_NAME FROM RESERVATION r JOIN MEDI_STAFF ms ON r.MEDICAL_STAFF_ID = ms.STAFF_ID";
             using (OracleConnection conn = new OracleConnection(strCon))
             {
@@ -143,23 +101,5 @@ namespace AdminProgram
                 ad.ShowDialog();
             }
         }
-
-        /*private void CorrectInfo_Btn(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("예약 수정 페이지로 이동합니다.");
-        }
-
-        private void DeleteInfo_Btn(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("예약 삭제 페이지로 이동합니다.");
-        }*/
-
-        /*private void reservationGrid_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if(e.ButtonState == MouseButtonState.Pressed)
-            {
-                MessageBox.Show("마우스 우 클릭 발생");
-            }
-        }*/
     }
 }
