f78168f68650cd6ef23a8a3d9908b903bade4066
devlee77 <tjdcks0928@naver.com>
2021-12-21 14:21:31 +0900

    insert기능 추가 완료

--- AdminProgram/AddPatient.xaml.cs
+++ AdminProgram/AddPatient.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -59,26 +60,6 @@ namespace AdminProgram
             }
         }
 
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void RadioButton_Copy_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void RadioButton_Copy1_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void RadioButton_Copy2_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         //취소버튼 클릭 이벤트
         private void Cancel_btn_Click(object sender, RoutedEventArgs e)
         {
@@ -89,6 +70,18 @@ namespace AdminProgram
                 Close();
         }
 
+        private bool NotOnlyNum(string str)
+        {
+            int i = 0;
+            for (int j = 0; j < str.Length; j++)
+            {
+                if ((str[j] >= 'a' && str[j] <= 'z') || (str[j] >= 'A' && str[j] <= 'Z'))
+                    return true;
+                j++;
+            }
+            return false;
+        }
+
         //입력이 잘되었는지 판단하는 함수
         private bool CheckRightValue()
         {
@@ -97,9 +90,9 @@ namespace AdminProgram
                 MessageBox.Show("이름을 입력해주세요.");
                 return true;
             }
-            else if (securityNum.Text == "" || securityNum.Text.Length != 13)
+            else if (securityNum.Text == "" || securityNum.Text.Length != 13 || NotOnlyNum(securityNum.Text))
             {
-                MessageBox.Show("주민번호는 13자리로 입력해주세요.");
+                MessageBox.Show("주민번호는 숫자 13자리로 입력해주세요.\nex)95092810XXXXX");
                 return true;
             }
             else if (address.Text == "")
@@ -107,7 +100,7 @@ namespace AdminProgram
                 MessageBox.Show("주소를 입력해주세요.");
                 return true;
             }
-            else if (phoneNum.Text == "")
+            else if (phoneNum.Text == "" || NotOnlyNum(phoneNum.Text))
             {
                 MessageBox.Show("핸드폰 번호를 입력해주세요.");
                 return true;
@@ -134,56 +127,71 @@ namespace AdminProgram
 
         private void InsertSQL()
         {
-            string? sql = null;
             OracleCommand comm = new();
 
-            sql = "INSERT INTO PATIENT (PATIENT_ID, " +
-                                       "PATIENT_NAME, " +
-                                       "RESIDENT_REGIST_NUM, " +
-                                       "ADDRESS, " +
-                                       "PHONE_NUM, " +
-                                       "GENDER, " +
-                                       "DOB, " +
-                                       "AGREE_OF_ALARM, " +
-                                       "HOME_NUM, " +
-                                       "REGIST_DATE) " +
-                        "VALUES (PATIENT_SEQ.NEXTVAL, " +
-                                ":name, " +
-                                ":securityNum, " +
-                                ":address, " +
-                                ":phoneNum, " +
-                                ":gender, " +
-                                ":date, " +
-                                ":smsCheck, " +
-                                ":homeNum, " +
-                                "'2017-09-28')";
+            comm.Connection = connn;
+            comm.CommandText = "INSERT INTO PATIENT(PATIENT_ID" +
+                                                    ", RESIDENT_REGIST_NUM" +
+                                                    ", PATIENT_NAME" +
+                                                    ", ADDRESS" +
+                                                    ", PHONE_NUM" +
+                                                    ", GENDER" +
+                                                    ", DOB" +
+                                                    ", AGREE_OF_ALARM" +
+                                                    ", HOME_NUM" +
+                                                    ", REGIST_DATE" +
+                                                    ", PATIENT_STATUS_VAL) " +
+                                "VALUES(PATIENT_SEQ.NEXTVAL" +
+                                        ", '" + securityNum.Text + "'" +
+                                        ", '" + patientName.Text + "'" +
+                                        ", '" + address.Text + "'" +
+                                        ", '" + phoneNum.Text + "'" +
+                                        ", '" + checkgen() + "'" +
+                                        ", To_Date('" + datePicker.Text + "', 'yyyy-MM-dd')" +
+                                        ", '" + checksms() + "'" +
+                                        ", '" + homeNum.Text + "'" +
+                                        ", To_Date('2021-12-20', 'yyyy-MM-dd')" +
+                                        ", 'T')";
+                                        
+            /*  
+                                        ", :securityNum" +
+                                        ", :name" +
+                                        ", :address" +
+                                        ", :phoneNum" +
+                                        ", :gender" +
+                                        ", To_Date(:date, 'yyyy-MM-dd')" +
+                                        ", :smsCheck" +
+                                        ", :homeNum" +
+                                        ", To_Date('2021-12-20', 'yyyy-MM-dd'))";
 
             comm.Parameters.Add(new OracleParameter("name", patientName.Text));
             comm.Parameters.Add(new OracleParameter("securityNum", securityNum.Text));
             comm.Parameters.Add(new OracleParameter("address", address.Text));
             comm.Parameters.Add(new OracleParameter("phoneNum", phoneNum.Text));
-            if (homeNum.Text == "")
-                comm.Parameters.Add(new OracleParameter("homeNum", null));
-            else
-                comm.Parameters.Add(new OracleParameter("homeNum", homeNum.Text));
-            if (smsCheck.IsChecked == true)
-                comm.Parameters.Add(new OracleParameter("smsCheck", "T"));
-            else
-                comm.Parameters.Add(new OracleParameter("smsCheck", "F"));
-            if (male.IsChecked == true)
-                comm.Parameters.Add(new OracleParameter("gender", "M"));
-            else
-                comm.Parameters.Add(new OracleParameter("gender", "F"));
-            comm.Parameters.Add(new OracleParameter("date", datePicker.Text));
-
-            
-            comm.Connection = connn;
-            comm.CommandText = sql;
+            comm.Parameters.Add(new OracleParameter("homeNum", homeNum.Text));
+            comm.Parameters.Add(new OracleParameter("smsCheck", checksms()));
+            comm.Parameters.Add(new OracleParameter("gender", checkgen()));
+            comm.Parameters.Add(new OracleParameter("date", datePicker.Text)); */
 
             // SQL문 지정 및 INSERT 실행
-            comm.ExecuteNonQuery(); //에러뜨는데 현재 , 문제라는데 나는 잘못한게 없음 아마도 들어는 값을 확인해봐야할것같다.
+            comm.ExecuteNonQuery();
             connn.Close();
-            connn = null;
+        }
+
+        private string checksms()
+        {
+            if (smsCheck.IsChecked == true)
+                return "T";
+            else
+                return "F";
+        }
+
+        private string checkgen()
+        {
+            if (male.IsChecked == true)
+                return "M";
+            else
+                return "F";
         }
 
         //저장버튼 클릭 이벤트
@@ -197,8 +205,10 @@ namespace AdminProgram
                     return;
 
                 ConnectDB();
-
+                
                 InsertSQL();
+
+                Close();
             }
         }        
     }
