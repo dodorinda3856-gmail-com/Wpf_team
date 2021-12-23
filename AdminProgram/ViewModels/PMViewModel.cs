6c38e1bd5f8d6e20452583aeb6e6edd7678b5ce1
devlee77 <tjdcks0928@naver.com>
2021-12-24 08:59:16 +0900

    환자관리 뷰모델 임시저장

--- AdminProgram/ViewModels/PMViewModel.cs
+++ AdminProgram/ViewModels/PMViewModel.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AdminProgram.ViewModels
 {
-    public class PMViewModel
+    public class PMViewModel : ObservableRecipient
     {
+        private readonly ILogger? logger;
+        string strCon = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=loonshot.cgxkzseoyswk.us-east-2.rds.amazonaws.com)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ORCL)));User Id=loonshot;Password=loonshot123;";
+
+        private ObservableCollection
+
     }
 }
