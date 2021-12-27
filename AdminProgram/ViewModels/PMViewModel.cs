033108e25b6cd3282bb35dadd5b012107255a6a9
devlee77 <tjdcks0928@naver.com>
2021-12-27 16:47:51 +0900

    임시저장

--- AdminProgram/ViewModels/PMViewModel.cs
+++ AdminProgram/ViewModels/PMViewModel.cs
@@ -11,26 +11,6 @@ namespace AdminProgram.ViewModels
 {
     public class PMViewModel : ObservableRecipient
     {
-        private readonly ILogger _logger;
-        string strCon = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=loonshot.cgxkzseoyswk.us-east-2.rds.amazonaws.com)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ORCL)));User Id=loonshot;Password=loonshot123;";
-
-
-        private ObservableCollection<PMModel> pMModel;
-        private ObservableCollection<PMModel> pMModels
-        {
-            get { return pMModel; }
-            set { SetProperty(ref pMModel, value); }
-        }
-
-        public PMViewModel(ILogger<PMViewModel> logger)
-        {
-            _logger = logger;
-            _logger.LogInformation("{@ILogger}", logger);
-
-            pMModels = new ObservableCollection<PMModel>();
-            pMModels.CollectionChanged += ContentCollectionChanged;
-        }
-
-
+        
     }
 }
