0cee3ec98be6d5368d535590f5332b6e77d0bc39
devlee77 <tjdcks0928@naver.com>
2021-12-29 17:44:23 +0900

    삭제버튼 이벤트 완료

--- AdminProgram/ModifiyPatient.xaml.cs
+++ AdminProgram/ModifiyPatient.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using AdminProgram.ViewModels;
+using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +23,8 @@ namespace AdminProgram
     /// </summary>
     public partial class ModifiyPatient : Window
     {
+        OracleConnection connn;
+
         //static으로 다른 클래스의 객체정보 받아올 변수
         private static PMModel Form2_value;
         public static PMModel Passvalue
@@ -42,14 +46,15 @@ namespace AdminProgram
                 male.IsChecked = true;
             else
                 female.IsChecked = true;
+            homeNum.Text = Passvalue.Home_Num;
         }
+
 
         public ModifiyPatient()
         {
             InitializeComponent();
             InitPatient();
         }
-
 
         //달력선택기능
         private void selectedDate(object sender, SelectionChangedEventArgs e)
@@ -63,6 +68,49 @@ namespace AdminProgram
             }
         }
 
+        private void ConnectDB()
+        {
+            try
+            {
+                string strCon = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=loonshot.cgxkzseoyswk.us-east-2.rds.amazonaws.com)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ORCL)));User Id=loonshot;Password=loonshot123;";
+                connn = new OracleConnection(strCon);
+                connn.Open();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+        }
+
+        //수정버전 sql
+        private void ModifiedInsertSQL()
+        {
+            OracleCommand comm = new();
+
+            comm.Connection = connn;
+            comm.CommandText = "update ";
+
+            //실행시키는기능
+            comm.ExecuteNonQuery();
+            connn.Close();
+        }
+        
+        //삭제버전 sql
+        private void DeleteInsertSQL()
+        {
+            OracleCommand comm = new();
+
+            comm.Connection = connn;
+            comm.CommandText = "update PATIENT " +
+                                "set PATIENT_STATUS_VAL = 'F' " +
+                                "where RESIDENT_REGIST_NUM = " + securityNum.Text;
+
+            //실행시키는기능
+            comm.ExecuteNonQuery();
+            connn.Close();
+        }
+
+
         //수정버튼 이벤트
         private void Modifiy_btn_Click(object sender, RoutedEventArgs e)
         {
@@ -70,19 +118,29 @@ namespace AdminProgram
 
             if (result == MessageBoxResult.Yes)
             {
+                
+
+                ConnectDB();
+
+                ModifiedInsertSQL();
+
                 MessageBox.Show("수정되었습니다.");
                 Close();
             }
         }
 
-        //취소버튼 이벤트
+
+        //삭제버튼 이벤트
         private void Delete_btn_Click(object sender, RoutedEventArgs e)
         {
             var result = MessageBox.Show("환자정보를 삭제하시겠습니까?", "삭제", MessageBoxButton.YesNo);
 
-            // If the no button was pressed ...
             if (result == MessageBoxResult.Yes)
             {
+                ConnectDB();
+
+                DeleteInsertSQL();
+
                 MessageBox.Show("해당 환자가 삭제되었습니다.");
                 Close();
             }
