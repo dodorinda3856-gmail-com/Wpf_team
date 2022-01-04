d284239e87cc48f342e6e4c1e52d14b8710f87bd
devlee77 <tjdcks0928@naver.com>
2022-01-04 15:10:25 +0900

    시술추가 xaml파일 cs파일 생성

---
+++ AdminProgram/AddProcedure.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AdminProgram
+{
+    /// <summary>
+    /// AddProcedure.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class AddProcedure : Window
+    {
+        public AddProcedure()
+        {
+            InitializeComponent();
+        }
+
+        //저장버튼
+        private void add_Procedure_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //취소버튼
+        private void cancel_add_Procedure_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
