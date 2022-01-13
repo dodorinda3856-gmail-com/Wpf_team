d56dfc43136e984e11f78a42239c0380d500bb7a
dodorinda <95510899+dodorinda@users.noreply.github.com>
2022-01-13 10:29:06 +0900

    메인 페이지 대기자 추가

--- AdminProgram/Models/TMTModel.cs
+++ AdminProgram/Models/TMTModel.cs
@@ -2,11 +2,15 @@
 
 namespace AdminProgram.Models
 {
-    public partial class TMTModel
+    public partial class TMTModel : ObservableObject
     {
         public string TREAT_TYPE { get; set; }
-        public int TREAT_COUNT { get; set;  }
-        
+        public int TREAT_COUNT { get; set; }
+
         public int WAIT_COUNT { get; set; }
+
+        public string PATIENTNAME { get; set; }
+
+        public string RESERVATIONDT { get; set; }
     }
 }
--- AdminProgram/StartPage.xaml.cs
+++ AdminProgram/StartPage.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using LiveCharts;
 using LiveCharts.Wpf;
 using AdminProgram.ViewModels;
+using AdminProgram.Models;
 
 namespace AdminProgram
 {
